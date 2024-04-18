59bac36a8a96eb402a4d16a6710dfcc6170d8ea4
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-18 22:37:27 +0530

    feat: add double count to counter

--- FirstApp/src/ButtonCounter.tsx
+++ FirstApp/src/ButtonCounter.tsx
@@ -1,8 +1,12 @@
 import {createSignal} from "solid-js";
+import {createEffect} from "solid-js";
 
 function ButtonCounter () {
 const [count, setCount] = createSignal<number>(0);
 
+createEffect(() => {
+  console.log("The count is now", count());
+});
 
   return (<><button onClick={() => setCount(count() + 1)}>Click Me</button>
     <div>Count : {count()}</div></>);
