02234b8f09b32219862306d309548f3b80e8b35e
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-20 21:46:19 +0530

    fix: add styles to styles.css

--- Bindings/src/App.tsx
+++ Bindings/src/App.tsx
@@ -4,11 +4,13 @@ import logo from "./logo.svg";
 import styles from "./App.module.css";
 import Events from "./Events";
 import Styles from "./Styles";
+import Classes from "./Classes";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
+        <Classes />
         <Events />
         <Styles />
       </header>
