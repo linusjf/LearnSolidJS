01bfe6e3ac726003c19780db46b465688883ad39
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-22 08:37:44 +0530

    fix: fix eslint violaton unused-var

--- eslint-yaml.config.cjs
+++ eslint-yaml.config.cjs
@@ -1,26 +1,27 @@
-const globals = require("globals");
 const yaml = require("eslint-plugin-yml");
 const yamlparser = require("yaml-eslint-parser");
 
-module.exports = [{
-  name: "ESLint for Yaml",
-  files: ["**/*.yaml"],
-  languageOptions: {
-    parserOptions: {
-      defaultYAMLVersion: "1.2"
+module.exports = [
+  {
+    name: "ESLint for Yaml",
+    files: ["**/*.yaml"],
+    languageOptions: {
+      parserOptions: {
+        defaultYAMLVersion: "1.2"
+      },
+      parser: yamlparser
     },
-    parser: yamlparser
-  },
-  linterOptions: {
-    noInlineConfig: false,
-    reportUnusedDisableDirectives: "error"
-  },
-  plugins: {
-    yaml
-  },
-  settings: {
-    yaml: {
-      version: "detect"
+    linterOptions: {
+      noInlineConfig: false,
+      reportUnusedDisableDirectives: "error"
+    },
+    plugins: {
+      yaml
+    },
+    settings: {
+      yaml: {
+        version: "detect"
+      }
     }
   }
-}];
+];
