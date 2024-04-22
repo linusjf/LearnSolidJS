9963858b3c93ada95a9a57da48ae6bcfee00fb29
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-22 22:40:29 +0530

    fix: add json config for eslint

--- eslint-html.config.cjs
+++ eslint-html.config.cjs
@@ -1,18 +1,16 @@
 const html = require("@html-eslint/eslint-plugin");
 const parser = require("@html-eslint/parser");
 
-module.exports = [
-  {
-    files: ["**/*.html"],
-    plugins: {
-      "@html-eslint": html
-    },
-    languageOptions: {
-      parser
-    },
-    rules: {
-      ...html.configs["flat/recommended"].rules,
-      "@html-eslint/indent": "error"
-    }
+module.exports = [{
+  files: ["**/*.html"],
+  plugins: {
+    "@html-eslint": html
+  },
+  languageOptions: {
+    parser
+  },
+  rules: {
+    ...html.configs["flat/recommended"].rules,
+    "@html-eslint/indent": "error"
   }
-];
+}];
---
+++ eslint-json.config.cjs
@@ -0,0 +1,19 @@
+const globals = require("globals");
+const jsonc = require("eslint-plugin-jsonc");
+
+const config = [
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/build/**"],
+    files: ["**/*.json"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  ...jsonc.configs["flat/recommended-with-json"]
+];
+
+module.exports = config;
